ed5ba425b5a8acf6dae1108461278c1354dd61da
Keith Ssewanonda <keith@laboremus.no>
2021-02-28 03:29:55 +0300

    feat:add seed data

---
+++ SavingPlatformApplication/Data/DatabaseInitializer/DatabaseInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SavingPlatformApplication.Data.DatabaseInitializer
+{
+    public class DatabaseInitializer
+    {
+        public static async Task SeedAsync(ApplicationDbContext context)
+        {
+            context.Database.Migrate();
+
+            if (!await context.Members.AnyAsync())
+            {
+                SeedMembers(context);
+                await context.SaveChangesAsync();
+            }
+            
+            if (!await context.SavingsGroups.AnyAsync())
+            {
+                SeedSavingsGroups(context);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private static void SeedMembers(ApplicationDbContext context)
+        {
+            context.Members.AddRange(GenerateFakeData.GetSampleMembers(150));
+            context.SaveChanges();
+        }
+
+        private static void SeedSavingsGroups(ApplicationDbContext context)
+        {
+            context.SavingsGroups.AddRange(GenerateFakeData.GetSampleSavingsGroups(150));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ SavingPlatformApplication/Data/DatabaseInitializer/GenerateFakeData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bogus;
+using SavingPlatformApplication.Data.Models;
+
+namespace SavingPlatformApplication.Data.DatabaseInitializer
+{
+    public class GenerateFakeData
+    {
+        public static List<Member> GetSampleMembers(int limit = 1)
+        {
+            var memberFaker = new Faker<Member>()
+                .RuleFor(c => c.FullName, f => f.Name.FullName())
+                .RuleFor(c => c.Email, f => f.Person.Email)
+                .RuleFor(c => c.PhoneNumber, f => f.Phone.PhoneNumberFormat())
+                .RuleFor(c => c.Balance, f => Convert.ToDouble(f.Finance.Amount(max: 1000000000)));
+
+            return memberFaker.Generate(limit);
+        }
+        
+        public static List<SavingsGroup> GetSampleSavingsGroups(int limit = 1)
+        {
+            var groupFaker = new Faker<SavingsGroup>()
+                .RuleFor(c => c.FullName, f => f.Name.FullName())
+                .RuleFor(c => c.Email, f => f.Person.Email)
+                .RuleFor(c => c.PhoneNumber, f => f.Phone.PhoneNumberFormat())
+                .RuleFor(c => c.Balance, f => Convert.ToDouble(f.Finance.Amount(max: 1000000000)))
+                .RuleFor(u => u.Members, f => GetSampleMembers(20));
+            
+            return groupFaker.Generate(limit);
+        }
+
+    }
+}
